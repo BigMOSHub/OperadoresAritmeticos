816591bc060d44679920c237b3a2008709cde809
Miguel Olivas <molivass@gmail.com>
2020-02-12 00:02:38 -0500

    1

---
+++ src/main/OperadoresAritmeticos.java
@@ -0,0 +1,24 @@
+package main;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		// Operadores atirmeticos
+	int variableX=50, variableY=10;
+	int resultado;
+	resultado = variableX + variableY;
+	System.out.println(resultado);
+	
+//	variableX++;
+//	variableX+=100;
+//	variableX-=100;
+//	variableX*=3;
+	variableX /=5; //variableX=variableX / 5
+
+	System.out.println(variableX);
+
+	
+	
+	}
+
+}
